5181e8ebc97b1d03039611d14b4ae7e342d32580
altail <2420206805z@gmail.com>
2023-03-05 21:03:22 +0800

    重新设置cpp

---
+++ C++primer-plus/build/CMakeFiles/4.22.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "4.22"
+  "4.22.pdb"
+  "CMakeFiles/4.22.dir/4.22.cpp.o"
+  "CMakeFiles/4.22.dir/4.22.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4.22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
